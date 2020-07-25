9a6ceecaf81a3a4eee3b4d6d47c40591c260c728
Jordan Gillard <jordan-gillard@outlook.com>
2020-07-25 11:18:47 -0500

    feat: Adds boomBang list comprehension function

--- src/baby.hs
+++ src/baby.hs
@@ -1,6 +1,5 @@
 doubleMe x = x + x
 doubleUs x y = x*2 + y*2
-doubleSmallNumber x = if x > 100
-                      then x
-                      else x*2
+doubleSmallNumber x = if x > 100 then x else x*2
 conanO'Brien = "It's a-me, Conan O'Brien!"
+boomBang xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
